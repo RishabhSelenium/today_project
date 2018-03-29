72496e8b816d014a7f57d74c1b5494e38b0676d1
om <om@om-PC>
2018-03-29 21:47:17 +0530

    I want to push the code on the server

---
+++ Apache_POI/src/pkg1/Class1.java
@@ -0,0 +1,40 @@
+package pkg1;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class Class1 {
+	
+	public static void main(String[] args) throws IOException 
+	{
+	File f=new File("C:\\Users\\om\\Downloads\\Maven.xlsx");
+	FileInputStream fi=new FileInputStream(f);
+	XSSFWorkbook xs=new XSSFWorkbook(fi);
+	XSSFSheet xt=xs.getSheetAt(0);
+	
+	int r=xt.getPhysicalNumberOfRows();
+	
+	for(int i=0;i<r;i++)
+	{
+		XSSFRow xr=xt.getRow(i);
+		for(int j=0;j<xr.getPhysicalNumberOfCells();j++)
+		{
+			XSSFCell xc=xr.getCell(j);
+			System.out.println(xc.getStringCellValue());
+		}
+	}
+	
+	
+	
+	
+	
+	}
+
+}
